c60608383ec6a2f8ea8027d199cc1118a8c6f7ac
AlexanderLukashuk <upiter465@gmail.com>
2022-01-28 17:04:31 +0600

    В StudentController создан конструктор и метод createStudent

--- src/com/company/controllers/StudentController.java
+++ src/com/company/controllers/StudentController.java
@@ -1,6 +1,24 @@
 package com.company.controllers;
 
+import com.company.entities.Student;
+import com.company.repositories.interfaces.IStudentRepository;
+
 public class StudentController
 {
-    
+    private final IStudentRepository repository;
+
+
+    public StudentController(IStudentRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public String creatStudent(String name, String surname, String group)
+    {
+        Student student = new Student(name, surname, group);
+
+        boolean created = repository.createStudent(student);
+
+        return (created ? "Student was created" : "Student creation was failed");
+    }
 }
