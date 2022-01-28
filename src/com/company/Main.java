396109e136c7b7e707eaacf8845791f9f2004f23
AlexanderLukashuk <upiter465@gmail.com>
2022-01-28 17:26:22 +0600

    Созданы экземпляры классов в Main и вызван метод start из MyApplication

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
-public class Main {
+import com.company.controllers.StudentController;
+import com.company.data.PostgresDB;
+import com.company.data.interfaces.IDB;
+import com.company.repositories.StudentRepository;
+import com.company.repositories.interfaces.IStudentRepository;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        IDB db = new PostgresDB();
+        IStudentRepository repo = new StudentRepository(db);
+        StudentController controller = new StudentController(repo);
+        MyApplication app = new MyApplication(controller);
+        app.start();
     }
 }
