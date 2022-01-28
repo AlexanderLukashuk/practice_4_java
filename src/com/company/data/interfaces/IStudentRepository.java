07d5652ae43ded6be3752ad05402437dca156d92
AlexanderLukashuk <upiter465@gmail.com>
2022-01-28 15:58:36 +0600

    В интерфейсе IStudentRepository созданы методы: createStudent, getStudent getAllStudents

--- src/com/company/data/interfaces/IStudentRepository.java
+++ src/com/company/data/interfaces/IStudentRepository.java
@@ -1,6 +1,12 @@
 package com.company.data.interfaces;
 
+import com.company.entities.Student;
+
+import java.util.List;
+
 public interface IStudentRepository
 {
-    
+    boolean createStudent(Student user);
+    Student getStudent(int id);
+    List<Student> getAllStudents();
 }
